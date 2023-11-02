0c5ea5742ee180299f68ba72c065c36d222f7156
deborahtalita <marpaung.deborah.tif20@polban.ac.id>
2023-11-02 10:35:51 +0700

    Initial project

---
+++ FootballPlayerWebApp/Controllers/PositionsController.cs
@@ -0,0 +1,29 @@
+﻿using FootballPlayerWebApp.Models;
+using FootballPlayerWebApp.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FootballPlayerWebApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PositionsController : ControllerBase
+    {
+        private readonly IPositionsService _positionService;
+
+        public PositionsController(IPositionsService positionService)
+        {
+            _positionService = positionService;
+        }
+
+        // GET: api/Positions
+        [HttpGet]
+        public async Task<IEnumerable<Position>> Get()
+        {
+            return await _positionService.GetPositionsList();
+        }
+    }
+}
